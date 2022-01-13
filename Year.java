a40b02c881ee9c0265767fefc06be5a0fd17584d
Muluken Taye <mulie@Mulukens-MBP.fios-router.home>
2022-01-13 00:34:44 -0500

    skeleton code for leap year computation

---
+++ Year.java
@@ -0,0 +1,10 @@
+public class Year {
+	public static void main(String[] args) {
+		// this gets the year from command line argument and
+		// converts string to int
+		int year = Integer.parseInt(args[0]);
+
+		// insert your code here
+
+	}
+}
